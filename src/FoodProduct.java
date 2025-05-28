7d812b8cdbfa91fe874cf6d9e67748dadd42e8c8
studente <giovanni.perra@branchina.edu.it>
2025-05-28 10:38:37 +0200

    e statata  creata la classe FoodProduct che si occupa di proddoti alimentari . è stato aggiunto qualche commento

--- src/FoodProduct.java
+++ src/FoodProduct.java
@@ -1,8 +1,17 @@
 package cashregister.src;
 
 public class FoodProduct extends Product {
+    /**
+     * la classe  FoodProduct parla di cibo è una classe pubblica
+     */
     private String expirationDate;
 
+    /**
+     * public FoodProduct e publica
+     * @param name
+     * @param price
+     * @param expirationDate
+     */
     public FoodProduct(String name, double price, String expirationDate) {
         super(name, price);
         this.expirationDate = expirationDate;
